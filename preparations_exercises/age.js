cc744c6ec3876f657ddfca6b72db27e2a4a00637
Daniela Mead <Daniela Mead>
2020-02-22 17:51:27 -0500

    just completed the chapter on variables. moving on to input/output now

---
+++ preparations_exercises/age.js
@@ -0,0 +1,7 @@
+let age = 26;
+
+console.log(`I am ${age} years old`);
+console.log(`In 10 years I will be ${age + 10}`);
+console.log(`In 20 years I will be ${age + 20}`);
+console.log(`In 30 years I will be ${age + 30}`);
+console.log(`In 40 years I will be ${age + 40}`);
---
+++ preparations_exercises/greeter.js
@@ -0,0 +1,9 @@
+const VICTOR = 'Victor';
+
+const greeter = (name) => {
+  console.log(`Good Morning, ${name}`);
+  console.log(`Good Afternoon, ${name}`);
+  console.log(`Good Evening, ${name}`)
+}
+
+greeter(VICTOR);
